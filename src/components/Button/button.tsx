d66293eeaf3c3a542230fd11495b452eca921118
AdaoBJr <adaobjr@gmail.com>
2022-04-15 08:37:16 -0400

    feat: :art: updated struture on Button Component

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,19 +1,24 @@
 import { ButtonHTMLAttributes } from 'react';
+import { useStyle } from '../../services/hooks';
 
 import defaultClasses from './button.module.css';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   name?: string;
   label?: string;
+  classes?: Object;
 }
 
 const button: React.FC<ButtonProps> = props => {
   const { id, type, label, children, ...restProps } = props;
+  const classes = useStyle({ defaultClasses, classes: props.classes });
 
   return (
-    <div>
-      <label htmlFor={id || label}>{label}</label>
-      <button id={id || label} {...restProps}>
+    <div className={classes.root}>
+      <label className={classes.label} htmlFor={id || label}>
+        {label}
+      </label>
+      <button id={id || label} className={classes.button} {...restProps}>
         {children}
       </button>
     </div>
